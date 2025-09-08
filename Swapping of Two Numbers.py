06cccada2259d5563edc49565bcff1d71f482947
keshav0099 <ckakeru7969@gmail.com>
2025-09-08 15:33:45 +0530

    Add files via upload

---
+++ Swapping of Two Numbers.py
@@ -0,0 +1,7 @@
+a= int(input("enter the first no :"))
+b= int(input("enter the second no :"))
+# Swap the values
+a, b = b, a
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
