79e870bfe25af57aae4a84c57256aae02d23251e
keshav0099 <ckakeru7969@gmail.com>
2025-09-08 15:23:12 +0530

    Add files via upload

---
+++ Area of Rectangle.py
@@ -0,0 +1,5 @@
+L= int(input("Enter the length of Rectangle :"))
+B= int(input("Enter the breadth of Rectangle :"))
+#Formula to calculate the Area of Rectangle
+A= L*B
+print("Area of Rectangle is ",A) 
