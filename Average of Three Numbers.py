0f35e636475d0c971492190bc2e11d79a150c4aa
keshav0099 <ckakeru7969@gmail.com>
2025-09-08 15:40:05 +0530

    Add files via upload

---
+++ Average of Three Numbers.py
@@ -0,0 +1,6 @@
+a= int(input("enter the first no. :"))
+b= int(input("enter the second no. :"))
+c= int(input("enter the third no. :"))
+#Calculate Average of three numbers
+Average= (a+b+c)/3
+print("Average of three no.", Average)
